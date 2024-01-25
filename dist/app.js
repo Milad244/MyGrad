fa549955f680d9cb22d47b9df0c56007d3601dc7
Milad244 <milad.abdi244@gmail.com>
2024-01-24 17:11:51 -0800

    Changed structure of all code and finished the setup part 1

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,7 @@
 "use strict";
-window.onload = function () {
-    LoadElements(yourGraduation.stage);
-};
+document.addEventListener('DOMContentLoaded', function () {
+    LoadStage();
+});
 function DisplayChanges(mode, elements, opacityLevel = 15) {
     if (Array.isArray(elements) === false) {
         elements = [elements];
@@ -37,6 +37,50 @@ function GiveError(reason = 'Not specified', num = 1) {
 function areElementsTruthy(elements) {
     return Object.values(elements).every(Boolean);
 }
+function usernameCheck(username) {
+    if (username.length <= 3 || username.replace(/\s/g, '').length <= 3) {
+        return {
+            isUsernameValid: false,
+            problemMsg: 'Username must have a minimum of 4 characters'
+        };
+    }
+    else if (username.length >= 12 || username.replace(/\s/g, '').length >= 12) {
+        return {
+            isUsernameValid: false,
+            problemMsg: 'Username must have less than 12 characters'
+        };
+    }
+    else {
+        const checkSameValueEl = document.getElementById('check-same-value');
+        if (checkSameValueEl) {
+            checkSameValueEl.innerHTML = username;
+            const nameDup = checkSameValueEl.innerHTML;
+            if (username != nameDup) {
+                return {
+                    isUsernameValid: false,
+                    problemMsg: 'Username contains an invalid character'
+                };
+            }
+        }
+        else {
+            GiveError('Not getting element', 4);
+        }
+        return {
+            isUsernameValid: true,
+            problemMsg: ''
+        };
+    }
+}
+var ElementIds;
+(function (ElementIds) {
+    ElementIds["startingPageContainer"] = "starting-page-container";
+    ElementIds["signUpButton"] = "js-signUp-button";
+    ElementIds["step1Container"] = "js-step-1-container";
+    ElementIds["step1Continue"] = "js-step1-continue";
+    ElementIds["step1UsernameInput"] = "js-step1-username-input";
+    ElementIds["step1UsernameError"] = "js-username-error";
+    ElementIds["step2Container"] = "js-step-2-container";
+})(ElementIds || (ElementIds = {}));
 const yourGraduation = {
     username: 'unnamed',
     courses: {},
@@ -44,51 +88,81 @@ const yourGraduation = {
     stage: 'signUp'
 };
 const yourCourses = yourGraduation.courses;
-function LoadElements(stage) {
-    if (stage === 'signUp') {
-        const startingPage = document.getElementById('starting-page-container');
-        const signUpButton = document.getElementById('js-signUp-button');
-        if (startingPage && signUpButton) {
-            DisplayChanges('just-display', [startingPage]);
-            signUpButton.addEventListener('click', () => {
-                DisplayChanges('no-display', [startingPage]);
-                yourGraduation.stage = 'setUp';
-                LoadElements(yourGraduation.stage);
-            });
-        }
-        else {
-            GiveError('Not getting element', 1);
-        }
+var Stages;
+(function (Stages) {
+    Stages["SignUp"] = "signUp";
+    Stages["SetUpPart1"] = "setUpPart1";
+    Stages["SetUpPart2"] = "setUpPart2";
+})(Stages || (Stages = {}));
+function LoadStage() {
+    const stage = yourGraduation.stage;
+    switch (stage) {
+        case Stages.SignUp:
+            handleSignUp();
+            break;
+        case Stages.SetUpPart1:
+            handleSetUpPart1();
+            break;
+        case Stages.SetUpPart2:
+            handleSetUpPart2();
+            break;
+        default:
+            GiveError('Invalid Stage', 1);
     }
-    else if (stage === 'setUp') {
-        const startingPage = document.getElementById('setup-page-container');
-        if (startingPage) {
-            DisplayChanges('just-display', [startingPage]);
-            if (Object.keys(yourCourses).length) {
-                Object.keys(yourCourses).forEach((value) => {
-                    const courseName = value;
-                    console.log(courseName);
-                    console.log(yourCourses[courseName]);
-                });
+}
+function handleSignUp() {
+    const signUpElements = {
+        startingPageContainer: document.getElementById(ElementIds.startingPageContainer),
+        signUpButton: document.getElementById(ElementIds.signUpButton)
+    };
+    if (areElementsTruthy(signUpElements)) {
+        DisplayChanges('just-display', [signUpElements.startingPageContainer]);
+        signUpElements.signUpButton.addEventListener('click', () => {
+            DisplayChanges('no-display', [signUpElements.startingPageContainer]);
+            yourGraduation.stage = 'setUpPart1';
+            LoadStage();
+        });
+    }
+    else {
+        GiveError('Not getting element', 1);
+    }
+}
+function handleSetUpPart1() {
+    const step1Elements = {
+        step1Container: document.getElementById(ElementIds.step1Container),
+        step1Continue: document.getElementById(ElementIds.step1Continue),
+        step1UsernameInput: document.getElementById(ElementIds.step1UsernameInput),
+        step1UsernameError: document.getElementById(ElementIds.step1UsernameError)
+    };
+    if (areElementsTruthy(step1Elements)) {
+        DisplayChanges('just-display', [step1Elements.step1Container]);
+        step1Elements.step1Continue.addEventListener('click', () => {
+            const newUsername = step1Elements.step1UsernameInput.value;
+            const usernameCheckReturn = usernameCheck(newUsername);
+            if (usernameCheckReturn.isUsernameValid) {
+                yourGraduation.username = newUsername;
+                DisplayChanges('no-display', [step1Elements.step1Container]);
+                yourGraduation.stage = 'setUpPart2';
+                LoadStage();
             }
             else {
-                const step1Elements = {
-                    step1: document.getElementById('js-step-1'),
-                    step1Continue: document.getElementById('js-step1-continue')
-                };
-                if (areElementsTruthy(step1Elements)) {
-                    DisplayChanges('just-display', [step1Elements.step1]);
-                    step1Elements.step1Continue.addEventListener('click', () => {
-                    });
-                }
-                else {
-                    GiveError('Not getting element', 2);
-                }
+                step1Elements.step1UsernameError.innerHTML = usernameCheckReturn.problemMsg;
             }
-        }
-        else {
-            GiveError('Not getting element', 3);
-        }
+        });
+    }
+    else {
+        GiveError('Not getting element', 2);
+    }
+}
+function handleSetUpPart2() {
+    const step2Elements = {
+        step2Container: document.getElementById(ElementIds.step2Container),
+    };
+    if (areElementsTruthy(step2Elements)) {
+        DisplayChanges('just-display', [step2Elements.step2Container]);
+    }
+    else {
+        GiveError('Not getting element', 2);
     }
 }
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
-window.onload = function(){
-    LoadElements(yourGraduation.stage);
-}
+document.addEventListener('DOMContentLoaded', function() {
+    LoadStage();
+});
 // General Functions
-function DisplayChanges(mode: string, elements: Element[], opacityLevel = 15): void{
+function DisplayChanges(mode: 'display' | 'no-display' | 'just-display' | 'disable' | 'enable', elements: Element[], opacityLevel = 15): void{
     if (Array.isArray(elements) === false){
         elements = [elements];
     }
@@ -36,6 +36,52 @@ function areElementsTruthy(elements: Record<string, HTMLElement | null>): boolea
     return Object.values(elements).every(Boolean);
 }
 
+function usernameCheck(username: string): {isUsernameValid: boolean, problemMsg: string}{
+    if (username.length <= 3 || username.replace(/\s/g, '').length <= 3){
+        return{
+            isUsernameValid: false,
+            problemMsg: 'Username must have a minimum of 4 characters'
+        }
+    } else if(username.length >= 12 || username.replace(/\s/g, '').length >= 12){
+        return{
+            isUsernameValid: false,
+            problemMsg: 'Username must have less than 12 characters'
+        } 
+    }else{
+        const checkSameValueEl = document.getElementById('check-same-value');
+        if(checkSameValueEl){
+            checkSameValueEl.innerHTML = username;
+            const nameDup = checkSameValueEl.innerHTML;
+        if (username != nameDup){
+            return{
+                isUsernameValid: false,
+                problemMsg: 'Username contains an invalid character'
+            }
+        }
+        } else{
+            GiveError('Not getting element', 4);
+        }
+        return{
+            isUsernameValid: true,
+            problemMsg: ''
+        }
+    }
+}
+
+// General Elements
+enum ElementIds {
+    // SignUpPage
+    startingPageContainer = 'starting-page-container',
+    signUpButton = 'js-signUp-button',
+    // setUpStep1
+    step1Container = 'js-step-1-container',
+    step1Continue = 'js-step1-continue',
+    step1UsernameInput = 'js-step1-username-input',
+    step1UsernameError = 'js-username-error',
+    // setUpStep2
+    step2Container = 'js-step-2-container',
+}
+
 // General Variables
 type yourGraduationType = {
     username: string;
@@ -53,47 +99,79 @@ const yourGraduation: yourGraduationType = {
     stage: 'signUp'
 };
 const yourCourses = yourGraduation.courses;
-// Function that loads different stages of the website
-function LoadElements(stage: string): void{
-    if (stage === 'signUp'){
-        const startingPage = document.getElementById('starting-page-container');
-        const signUpButton = document.getElementById('js-signUp-button');
-        if (startingPage && signUpButton){
-            DisplayChanges('just-display', [startingPage]);
-            signUpButton.addEventListener('click', () => {
-                DisplayChanges('no-display', [startingPage]);
-                yourGraduation.stage = 'setUp';
-                LoadElements(yourGraduation.stage);
+
+enum Stages {
+    SignUp = 'signUp',
+    SetUpPart1 = 'setUpPart1',
+    SetUpPart2 = 'setUpPart2',
+}
+
+function LoadStage(): void{
+    const stage = yourGraduation.stage;
+    switch (stage) {
+        case Stages.SignUp:
+            handleSignUp();
+            break;
+        case Stages.SetUpPart1:
+            handleSetUpPart1();
+            break;
+        case Stages.SetUpPart2:
+            handleSetUpPart2();
+            break;
+        default: 
+            GiveError('Invalid Stage', 1);
+    }
+}
+
+function handleSignUp(){
+    const signUpElements = {
+        startingPageContainer: document.getElementById(ElementIds.startingPageContainer),
+        signUpButton: document.getElementById(ElementIds.signUpButton)
+    }
+    if (areElementsTruthy(signUpElements)){
+        DisplayChanges('just-display', [signUpElements.startingPageContainer!]);
+        signUpElements.signUpButton!.addEventListener('click', () => {
+            DisplayChanges('no-display', [signUpElements.startingPageContainer!]);
+            yourGraduation.stage = 'setUpPart1';
+            LoadStage();
+        })
+    } else{
+        GiveError('Not getting element', 1);
+    }
+}
+function handleSetUpPart1(){
+        const step1Elements = {
+            step1Container: document.getElementById(ElementIds.step1Container),
+            step1Continue: document.getElementById(ElementIds.step1Continue),
+            step1UsernameInput: document.getElementById(ElementIds.step1UsernameInput) as HTMLInputElement,
+            step1UsernameError: document.getElementById(ElementIds.step1UsernameError)
+        }
+        if (areElementsTruthy(step1Elements)){
+            DisplayChanges('just-display', [step1Elements.step1Container!]);
+            step1Elements.step1Continue!.addEventListener('click', () => {
+                const newUsername = step1Elements.step1UsernameInput!.value
+                const usernameCheckReturn = usernameCheck(newUsername);
+                if (usernameCheckReturn.isUsernameValid){
+                    yourGraduation.username = newUsername;
+                    DisplayChanges('no-display', [step1Elements.step1Container!]);
+                    yourGraduation.stage = 'setUpPart2';
+                    LoadStage();
+                } else {
+                    step1Elements.step1UsernameError!.innerHTML = usernameCheckReturn.problemMsg;
+                }
             })
         } else{
-            GiveError('Not getting element', 1);
+            GiveError('Not getting element', 2);
         }
-    } else if(stage === 'setUp'){
-        const startingPage = document.getElementById('setup-page-container');
-        if (startingPage){
-            DisplayChanges('just-display', [startingPage]);
-            if (Object.keys(yourCourses).length){
-                Object.keys(yourCourses).forEach((value: string) => {
-                    const courseName = value;
-                    console.log(courseName);
-                    console.log(yourCourses[courseName]);
-                });
-            } else{
-                const step1Elements = {
-                    step1: document.getElementById('js-step-1'),
-                    step1Continue: document.getElementById('js-step1-continue')
-                }
-                if (areElementsTruthy(step1Elements)){
-                    DisplayChanges('just-display', [step1Elements.step1!]);
-                    step1Elements.step1Continue!.addEventListener('click', () => {
-                        
-                    })
-                } else{
-                    GiveError('Not getting element', 2);
-                }
-            }
-        } else{
-            GiveError('Not getting element', 3);
-        }
+}
+function handleSetUpPart2(){
+    const step2Elements = {
+        step2Container: document.getElementById(ElementIds.step2Container),
+    }
+    if (areElementsTruthy(step2Elements)){
+        DisplayChanges('just-display', [step2Elements.step2Container!]);
+        //code to pick your courses
+    } else{
+        GiveError('Not getting element', 2);
     }
 }
